ac8e76fba98d30cb5e9d1fd11535f9043e66e715
huness <tialdorf@gmail.com>
2018-06-26 15:35:06 +0300

    file structure added

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    plumber = require("gulp-plumber"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    mqpack = require("css-mqpacker"),
+    min = require("gulp-csso"),
+    rename = require("gulp-rename"),
+    imgmin = require("gulp-imagemin"),
+    run = require("run-sequence"),
+    del = require("del");
+
+gulp.task("style", function() {
+    gulp.src("sass/style.scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(postcss([
+            autoprefixer({browsers: [
+                "last 1 version",
+                "last 2 Chrome versions",
+                "last 2 Firefox versions",
+                "last 2 Opera versions",
+                "last 2 Edge versions"
+            ]}),
+            mqpack({
+                sort: true
+            })
+        ]))
+        .pipe(gulp.dest("build/css"))
+        .pipe(min())
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest("build/css"))
+});
+
+gulp.task("html", function() {
+    gulp.src("index.html")
+        
+        .pipe(gulp.dest("build"));
+});
+
+gulp.task("js", function() {
+    gulp.src("js/script.js")
+
+        .pipe(gulp.dest("build/js"));
+})
+
+gulp.task("images", function() {
+    return gulp.src("img/**/*.{png,jpg,gif}")
+                .pipe(imgmin([
+                    imgmin.optipng({optimizationLevel: 3}),
+                    imgmin.jpegtran({progressive: true})
+                ]))
+                .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("del", function() {
+    return del("build");
+});
+
+gulp.task("build", function(fn) {
+    run("del", "style", "html", "js", "images", fn);
+});
+
+gulp.task("watch", function() {
+    gulp.watch("index.html", ["build"]);
+    gulp.watch("sass/**/*.scss", ["build"]);
+    gulp.watch("js/**/*.js", ["build"]);
+});
