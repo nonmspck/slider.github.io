65a17bccae6b45027afe73bfa5cd4b0b61d3276a
huness <tialdorf@gmail.com>
2018-06-27 17:37:15 +0300

    +

--- build/js/script.js
+++ build/js/script.js
@@ -1,19 +1,9 @@
 $(document).ready(function () {
     $(".slider").slick({
         infinite: true,
-        slidesToShow: 2,
         slidesToScroll: 1,
         arrows: false,
-        variableWidth: true,
-        responsive: [
-            {
-                breakpoint: 950,
-                settings: {
-                    slidesToShow: 1,
-                    variableWidth: false
-                }
-            }
-        ]
+        variableWidth: true
     }).on("wheel", function (event) {
         event.preventDefault();
         if (event.originalEvent.deltaY < 0) {
@@ -21,7 +11,7 @@ $(document).ready(function () {
           } else {
             $(this).slick('slickPrev');
           }}).on("beforeChange", function (event, slick, currentSlide, nextSlide) {
-        if ( slick.slideCount > nextSlide + $(this).slick( "slickGetOption", "slidesToShow" ) ) {
+        if ( slick.slideCount > 2 + nextSlide + $(this).slick( "slickGetOption", "slidesToShow" ) ) {
             $(this).removeClass("slider-last");
         } else {
             $(this).addClass("slider-last");
